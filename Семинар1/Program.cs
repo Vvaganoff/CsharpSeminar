4d937ff50616b569ff09e69b0f62b7e24e0e384e
«Vadim <vvaganoff@mail.ru>
2022-11-11 21:40:38 +0400

    Семинар1

---
+++ Семинар1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number: ");
+
+int Number = Convert.ToInt32( Console.ReadLine() );
+
+Console.WriteLine( "Square " + Number + " is " + (Number * Number) );
+
+
