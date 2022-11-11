e74680487e9a274bd6e6cd17ace37167088f6855
«Vadim <vvaganoff@mail.ru>
2022-11-11 22:14:35 +0400

    задача 3

--- Семинар1/Program.cs
+++ Семинар1/Program.cs
@@ -1,13 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Input 2 numbers: ");
+Console.WriteLine("Введите день недели: ");
 
 int Number1 = Convert.ToInt32( Console.ReadLine() );
-int Number2 = Convert.ToInt32( Console.ReadLine() );
-if (Number1 == Number2 * Number2) {
-    Console.WriteLine( "Yes" );
-} else
+
+if (Number1 == 1) {
+    Console.WriteLine( "Понедельник" );
+} else if (Number1 == 2)
 {
-    Console.WriteLine( "No" );
-}
+    Console.WriteLine( "Вторник" );
+} else if (Number1 == 2)
+{
+    Console.WriteLine( "Вторник" );}
+     else if (Number1 == 3)
+{
+    Console.WriteLine( "Среда" );}
+ else if (Number1 == 4)
+{
+    Console.WriteLine( "Четверг" );
+} else if (Number1 == 5)
+{
+    Console.WriteLine( "Пятница" );
+  }  else if (Number1 == 6)
+{
+    Console.WriteLine( "Суббота" );}
+    else if (Number1 == 7)
+{
+    Console.WriteLine( "Воскресение" );}
+    else {Console.WriteLine( "Нет такого дня" );}
 
 
