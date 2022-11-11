9135f3df51b241ff3a7442ec667645d5d601b047
«Vadim <vvaganoff@mail.ru>
2022-11-11 23:01:03 +0400

    задача 7

---
+++ Семинар1/Задача5/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine( "Введите число" );
+
+int UserNumber = Convert.ToInt32( Console.ReadLine() );
+
+for (int i = -UserNumber; i<=UserNumber; i++) 
+{Console.WriteLine(i);}
+
---
+++ Семинар1/Задача7/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine( "Введите число" );
+
+int UserNumber = Convert.ToInt32( Console.ReadLine() );
+
+{Console.WriteLine(%"" UserNumber % 10);}
